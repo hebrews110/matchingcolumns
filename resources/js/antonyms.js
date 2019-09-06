79a14d39e6af6b92b67901e68074c23174c755e2
Lee Giles <hebrews110@gmail.com>
2019-09-06 10:33:23 -0400

    Add antonyms.html.

---
+++ resources/js/antonyms.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['small', 1], ['day', 2], ['sad', 5], ['hard', 3], ['low', 4], ['yes', 6]];
+var cards2 = [['huge, big, enormous', 1], ['excited, happy, glad', 5], ['night, evening', 2], ['soft, easy', 3], ['high', 4], ['no', 6]];
