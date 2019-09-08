9bbdf532b0bbe6ae729df493ea4800f84806e509
Lee Giles <hebrews110@gmail.com>
2019-09-08 14:35:14 -0400

    Create insect.html.

---
+++ resources/js/insects.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['jumping legs', 1], ['anatennae', 2], ['compound eyes', 5], ['simple eyes', 3], ['palpi', 4], ['spiracles', 6]];
+var cards2 = [['propel cricket', 1], ['see in different directions', 5], ['sense touch and odor', 2], ['see light and dark', 3], ['grasp food', 4], ['holes to enable breathing', 6]];
