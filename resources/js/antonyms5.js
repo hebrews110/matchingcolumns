4cb8aa672649f5af499fd39bd080c34b3343726b
Lee Giles <hebrews110@gmail.com>
2019-09-10 11:36:36 -0400

    Add antonyms5.

---
+++ resources/js/antonyms5.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['inferior', 1], ['fertile', 2], ['cruel', 5], ['stingy', 3], ['shrink', 4], ['temporary', 6]];
+var cards2 = [['superior', 1], ['merciful', 5], ['barren', 2], ['generous', 3], ['expand', 4], ['permanent', 6]];
