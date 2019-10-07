295260aa51e4e99d2a295a9b056aae23c412e13b
Lee Giles <hebrews110@gmail.com>
2019-10-07 14:22:35 -0400

    Add suffix. Fix play again.

---
+++ resources/js/suffix.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['-less', 1], ['-ism', 2], ['-ful', 3], ['-ology', 4], ['-tion', 5], ['-ship', 6]];
+var cards2 = [['without', 1], ['belief', 2], ['with', 3], ['the study of', 4], ['quality, state of being', 5], ['position held', 6]];
