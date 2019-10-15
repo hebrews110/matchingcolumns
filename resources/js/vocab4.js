955be7ba713a47579c9259e014d1dafad2689ffb
Lee Giles <hebrews110@gmail.com>
2019-10-15 14:43:00 -0400

    Add vocab4.

---
+++ resources/js/vocab4.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['dreary', 1], ['experienced', 2], ['blizzard', 3], ['rugged', 4], ['victorious', 5], ['courageous', 6], ['comrade', 7], ['rouse', 8], ['illuminate', 9], ['catapult', 10]];
+var cards2 = [['gloomy, cheerless', 1], ['having done something before', 2], ['a heavy snowstorm', 3], ['a rough surface', 4], ['having won', 5], ['fearless', 6], ['companion', 7], ['to awaken or to stir up a strong feeling', 8], ['to light up', 9], ['to hurl or launch', 10]];
