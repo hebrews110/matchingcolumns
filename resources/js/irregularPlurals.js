935a3417c1bcf22a528b939b4d7b8e8efefebabb
Lee Giles <hebrews110@gmail.com>
2019-10-21 11:32:24 -0400

    Create irregular plurals.

---
+++ resources/js/irregularPlurals.js
@@ -0,0 +1,8 @@
+// to be paired with script.js in column
+
+var cards1 = [
+  ['mouse', 1], ['child', 2], ['person', 3], ['man', 4], ['goose', 5], ['die', 6]
+];
+var cards2 = [
+  ['mice', 1], ['children', 2], ['people', 3], ['men', 4], ['geese', 5], ['dice', 6]
+];
