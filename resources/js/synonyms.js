cb369f02db7261015456b58bfda6352b8275c697
Lee Giles <hebrews110@gmail.com>
2019-11-20 17:12:06 -0500

    Add synonyms.

---
+++ resources/js/synonyms.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['small', 1], ['happy', 2], ['sad', 5], ['hard', 3], ['big', 4], ['start', 6]];
+var cards2 = [['little', 1], ['gloomy', 5], ['glad', 2], ['difficult', 3], ['large', 4], ['begin', 6]];
