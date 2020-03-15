009c4932a313df8bff142fd4f7d36ad920e73a76
Lee Giles <hebrews110@gmail.com>
2020-03-15 13:45:10 -0400

    Add gesturesJapan.

---
+++ resources/js/gesturesJapan.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['<img class="medium" src="./resources/images/nose.png"/>', 1], ['<img class="medium" src="./resources/images/horns.png"/>', 2], ['<img class="medium" src="./resources/images/hand.png"/>', 3], ['<img class="medium" src="./resources/images/ok.png"/>', 4], ['<img class="medium" src="./resources/images/xArms.png"/>', 5]];
+var cards2 = [['me', 1], ['angry', 2], ['come here', 3], ['money', 4], ['no good', 5]];
