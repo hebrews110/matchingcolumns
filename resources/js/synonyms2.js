d086f283bfe9e693939ebd7c9b22b9ef84295007
Lee Giles <hebrews110@gmail.com>
2020-04-20 16:35:13 -0400

    High school synonyms

---
+++ resources/js/synonyms2.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['tempestuous', 1], ['omnipotent', 2], ['vehement', 3], ['loquacious', 4], ['usurp', 5], ['sanguine', 6]];
+var cards2 = [['turbulent', 1],  ['powerful', 2], ['fervent', 3], ['verbose', 4], ['seize', 5], ['optimistic', 6]];
