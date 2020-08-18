f5326643136f0383663a5ae60520b37a2899a025
Lee Giles <hebrews110@gmail.com>
2020-08-18 12:11:54 -0400

    Add 3D shapes.

---
+++ resources/js/3Dshapes.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['<img class="medium" src="./resources/images/cube.png"/>', 1], ['<img class="medium" src="./resources/images/cylinder.png"/>', 2], ['<img class="medium" src="./resources/images/sphere.png"/>', 3], ['<img class="medium" src="./resources/images/cone.png"/>', 4], ['<img class="medium" src="./resources/images/pyramid.png"/>', 5]];
+var cards2 = [['cube', 1], ['cylinder', 2], ['sphere', 3], ['cone', 4], ['pyramid', 5]];
