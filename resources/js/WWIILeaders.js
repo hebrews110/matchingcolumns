b1bfcd08b10f4fee2f51aa471bbaf5204dcf853f
Lee Giles <hebrews110@gmail.com>
2020-09-27 12:00:22 -0400

    Adding WWII Leaders.

---
+++ resources/js/WWIILeaders.js
@@ -0,0 +1,4 @@
+//antonyms to be paired with script.js in column
+
+var cards1 = [['United States', 1], ['Italy', 2], ['Germany', 3], ['Great Britain', 4], ['Russia', 5], ['Japan', 6]];
+var cards2 = [['Franklin D. Roosevelt', 1], ['Benito Mussolini', 2], ['Adolf Hitler', 3], ['Winston Churchhill', 4], ['Joseph Stalin', 5], ['Emperor Hirohito', 6]];
