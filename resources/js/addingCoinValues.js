19cec446d4bba36582082d43735416a09290986a
Lee Giles <hebrews110@gmail.com>
2021-08-03 15:22:54 -0400

    edits

--- resources/js/addingCoinValues.js
+++ resources/js/addingCoinValues.js
@@ -1,4 +1,4 @@
 //antonyms to be paired with script.js in column
 
-var cards1 = [['$0.47', 1], ['$0.59', 2], ['$0.73', 3], ['$0.95', 4], ['$0.68', 4]];
-var cards2 = [['2 dimes + 5 nickels + 2 pennies', 1], ['1 quarter + 3 dimes + 4 pennies', 2], ['2 quarters + 3 nickels + 8 pennies', 3], ['1 quarter + 2 dimes + 4 nickels + 3 pennies', 4]];
+var cards1 = [['$0.47', 1], ['$0.59', 2], ['$0.73', 3], ['$0.95', 4], ['$0.68', 5]];
+var cards2 = [['2 dimes + 5 nickels + 2 pennies', 1], ['1 quarter + 3 dimes + 4 pennies', 2], ['2 quarters + 3 nickels + 8 pennies', 3], ['1 quarter + 4 dimes + 5 nickels + 5 pennies', 4], ['1 quarter + 2 dimes + 4 nickels + 3 pennies', 5]];
